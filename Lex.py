65cc3e2555d93f14ed2ed28440b46cde049ad66e
hajarUwUber <119138151+hajarUwUber@users.noreply.github.com>
2023-12-24 18:17:48 +0100

    Update Lex.py

--- Lex.py
+++ Lex.py
@@ -1,8 +1,8 @@
 from ply import lex
 
 # Define the token names
-tokens = ('ISM_MAWSOL', 'FI3L1', '7ARF_JAR1', 'ISM_MAJROR', 'HAE','FI3L2','7ARF_JAR2','MOBTADA2','KHABAR',
-          '7AL','FI3L3','MAF3OL_BIH','NA2IB_DARF','ISM_MAJROR2','FA2','ADAT_CHART','FI3L4','FA3IL','FI3L5','YA2')
+tokens = ('ISM_MAWSOL', 'FI3L1', '7ARF_JAR1', 'ISM_MAJROR', 'HAE', 'FI3L2', '7ARF_JAR2', 'MOBTADA2', 'KHABAR', '7AL',
+          'FI3L3', 'MAF3OL_BIH', 'NA2IB_DARF', 'ISM_MAJROR2', 'FAE', 'FI3L4', 'FA3IL', 'FI3L5', 'YAE','ADAT_CHART')
 
 # Define the regular expressions for each token
 t_ISM_MAWSOL = r'من'
@@ -19,12 +19,12 @@ t_FI3L3=r'أعلم'
 t_MAF3OL_BIH=r'الرماية'
 t_NA2IB_DARF=r'كل'
 t_ISM_MAJROR2=r'يوم'
-t_FA2=r'ف'
+t_FAE=r'ف'
 t_ADAT_CHART=r'لما'
 t_FI3L4=r'اشتد'
 t_FA3IL=r'ساعد'
 t_FI3L5=r'رمان'
-t_YA2=r'ي'
+t_YAE=r'ي'
 # Ignore whitespace
 t_ignore = ' \t\n'
 
@@ -32,7 +32,6 @@ t_ignore = ' \t\n'
 def t_error(t):
     print(f"Illegal character: {t.value[0]}")
     t.lexer.skip(1)
-
 # Build the lexer
 lexer = lex.lex()
 
